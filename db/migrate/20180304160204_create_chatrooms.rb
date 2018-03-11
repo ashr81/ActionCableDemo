118e942169d10f3c527d3ced3fcf41c0cdffab01
ashr81 <ashrithreddi81@gmail.com>
2018-03-11 14:44:58 +0530

    Added all the config files and made working changes.

--- db/migrate/20180304160204_create_chatrooms.rb
+++ db/migrate/20180304160204_create_chatrooms.rb
@@ -2,6 +2,8 @@ class CreateChatrooms < ActiveRecord::Migration[5.1]
   def change
     create_table :chatrooms do |t|
 
+      t.string :topic
+      t.string :slug
       t.timestamps
     end
   end
--- db/migrate/20180304160237_create_messages.rb
+++ db/migrate/20180304160237_create_messages.rb
@@ -2,6 +2,9 @@ class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
 
+      t.integer :user_id
+      t.integer :chatroom_id
+      t.bson :content
       t.timestamps
     end
   end
